6991d7176411f6cdf2577f2382f0404f88dd5c34
Nguyen Minh Tho <definedentity@proton.me>
2024-10-27 17:13:57 +0700

    first commit

---
+++ src/main/kotlin/dev/definedentity/definedsblocks/DefinedsBlocksDataGenerator.kt
@@ -0,0 +1,9 @@
+package dev.definedentity.definedsblocks
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator
+
+object DefinedsBlocksDataGenerator : DataGeneratorEntrypoint {
+	override fun onInitializeDataGenerator(fabricDataGenerator: FabricDataGenerator) {
+	}
+}
